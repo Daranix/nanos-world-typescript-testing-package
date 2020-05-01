c340d8073109c26d93a480c2c711a43a32d32d05
Daranix <daranixlion@gmail.com>
2020-05-01 23:25:55 +0200

    Add missing alpha parameter on color class.

--- src/typings/nanos-world/utility/color.d.ts
+++ src/typings/nanos-world/utility/color.d.ts
@@ -4,6 +4,7 @@ declare class Color {
     R: number;
     G: number;
     B: number;
+    A: number;
 
-    constructor(r: number, g: number, b: number);
+    constructor(r: number, g: number, b: number, alpha: number);
 }
